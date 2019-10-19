e7e32ee260bce52c597141a84f42bf182cfdd808
Carlos Arturo <carlos@falconmasters.com>
2019-10-18 19:04:42 -0500

    Actualizando repo

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const elementosCarousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel, {
+    duration: 150,
+    dist: -80,
+    shift: 5,
+    padding: 5,
+    numVisible: 3,
+    indicators: true,
+    noWrap: false
+  });
+});
